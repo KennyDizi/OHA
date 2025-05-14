303e86c9b0723352c06f820583142728b70de067
Trung Dinh <xamarindevelopervietnam@outlook.com>
2025-05-14 22:27:04 +0700

    Update shell scripts

--- update-open-hands.sh
+++ update-open-hands.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker pull docker.all-hands.dev/all-hands-ai/runtime:latest-nikolaik
+docker pull docker.all-hands.dev/all-hands-ai/runtime:0.38-nikolaik
+docker pull docker.all-hands.dev/all-hands-ai/openhands:0.38
