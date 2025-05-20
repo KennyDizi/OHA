e25d275f12f00ed98d40cc16752de3169bc2d9ae
Trung Dinh <xamarindevelopervietnam@outlook.com>
2025-05-20 22:42:32 +0700

    v0.39

--- update-open-hands.sh
+++ update-open-hands.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker pull docker.all-hands.dev/all-hands-ai/runtime:0.38-nikolaik
-docker pull docker.all-hands.dev/all-hands-ai/openhands:0.38
+docker pull docker.all-hands.dev/all-hands-ai/runtime:0.39-nikolaik
+docker pull docker.all-hands.dev/all-hands-ai/openhands:0.39
