fe508788c8091d3bfa45133dc934a9edcab8c4a8
Trung Dinh <xamarindevelopervietnam@outlook.com>
2025-05-24 07:42:20 +0700

    Pull `latest` image version

--- run-open-hands.sh
+++ run-open-hands.sh
@@ -33,7 +33,7 @@ if [ -f .env ]; then
 
     # Run the Open Hands container
     docker run -it --rm --pull=always \
-        -e SANDBOX_RUNTIME_CONTAINER_IMAGE=docker.all-hands.dev/all-hands-ai/runtime:0.39.1-nikolaik \
+        -e SANDBOX_RUNTIME_CONTAINER_IMAGE=docker.all-hands.dev/all-hands-ai/runtime:latest-nikolaik \
         -e LOG_ALL_EVENTS=true \
         -e SANDBOX_USER_ID=$(id -u) \
         -e RUNTIME_MOUNT=$RUNTIME_MOUNT \
@@ -54,7 +54,7 @@ if [ -f .env ]; then
         --add-host host.docker.internal:host-gateway \
         --network oha-network \
         --name "${CONTAINER_NAME}" \
-        docker.all-hands.dev/all-hands-ai/openhands:0.39.1 \
+        docker.all-hands.dev/all-hands-ai/openhands:latest \
         python3 -m openhands.cli.main
 else
     echo "Error: .env file not found"
